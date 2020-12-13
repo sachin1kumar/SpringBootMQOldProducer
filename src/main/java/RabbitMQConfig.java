c86337e7532d5ee75ee6ccc44830a1b75e460db1
Sachin <sachinkmr375@gmail.com>
2020-12-13 11:56:08 +0530

    first commit

--- src/main/java/RabbitMQConfig.java
+++ src/main/java/RabbitMQConfig.java
@@ -1,2 +1,50 @@
-package PACKAGE_NAME;public class RabbitMQConfig {
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+
+@Configuration
+@EnableAutoConfiguration
+public class RabbitMQConfig
+{
+    final static String queueName = "spring_message_queue";
+
+    @Autowired
+    RabbitTemplate rabbitTemplate;
+
+    @Bean
+    public ConnectionFactory connectionFactory()
+    {
+        CachingConnectionFactory connectionFactory = new CachingConnectionFactory("localhost");
+        connectionFactory.setPort(5672);
+        connectionFactory.setUsername("guest");
+        connectionFactory.setPassword("guest");
+        return connectionFactory;
+    }
+
+    @Bean
+    Queue queue()
+    {
+        return new Queue(queueName, false);
+    }
+
+    @Bean
+    TopicExchange exchange()
+    {
+        return new TopicExchange("message_queue_exchange");
+    }
+
+    @Bean
+    Binding binding(Queue queue, TopicExchange exchange)
+    {
+        return BindingBuilder.bind(queue).to(exchange).with(queueName);
+    }
+
 }
